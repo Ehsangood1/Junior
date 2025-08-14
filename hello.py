7472fc00f327aa88c60de6796d5024cf96fec5d5
Ehsan <ehsangood9@gmail.com>
2025-08-14 16:27:34 +0200

    adding some features

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print ("hello there!")
+name="Ehsan"
+age = 40
+height = 2.00
+is_coder = True
+
+
+print("My name is", name)
+print("Age:", age)
+print("Height:", height, "m")
+print("Am I a coder?", is_coder)
