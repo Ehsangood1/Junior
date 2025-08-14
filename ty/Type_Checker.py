df8ce3d5dedaf5898a4525dcca33b20151de6079
Ehsan <ehsangood9@gmail.com>
2025-08-14 18:49:02 +0200

    analysing type

---
+++ ty/Type_Checker.py
@@ -0,0 +1,2 @@
+user_input = input ("Enter your input then we check it what it is :)")
+print ("You entered:", type(user_input))
--- variable_combat.py
+++ variable_combat.py
@@ -1,3 +1,3 @@
-name = int (input (("Enter your age, then i tell you when you born:")))
-year_of_birth = 2025 - name
+age = int (input (("Enter your age, then i tell you when you born:")))
+year_of_birth = 2025 - age
 print("You were born in:", year_of_birth)
